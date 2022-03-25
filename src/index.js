250816f810652df5f3b81e62ce035350d96de38d
Laís Farias <fariaslais399@hotmail.com>
2022-03-25 17:50:03 -0300

    Terminamos a apostila 03, criando um componente React e estudando JSX

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import  React  from "react"
 import ReactDOM from "react-dom"
+import './styles.css' 
 
 // const App = () => {
 //     return <div>Meu primeiro componente ReactJS</div>
 // }
 
 const App = () => {
+    const estilosBotao = {
+        marginTop: 12, 
+        paddingTop: 8, 
+        paddingBottom: 8, 
+        backgroundColor: 'blueviolet', 
+        color: 'white',
+        border: 'none', 
+        width: "100%", 
+        borderRadius: 8
+    }
+
+    const textoDoRotulo = 'Nome:'
+
+    const obterTextoDoBotao = () => "Enviar"
     return(
+        // Usamos { } para indicar que o que vem a seguir é uma expressão Javascript. Quando usamos { } pela segunda vez, estamos especificando
+        //um objeto JSON.
         <div style={{margin: 'auto', width: 768, backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
-            <label htmlFor="nome" style={{display: 'block', marginBottom: 4}}>Nome:</label>
+            <label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
             <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 
-                'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
-            <button style={{marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: 
-                'blueviolet', color: 'white', border: 'none', width: '100%', borderRadius: 8}}>Enviar</button>
+            'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
+            <button style={estilosBotao}>{obterTextoDoBotao()}</button>
         </div>
     ) 
 }
